c6fbc9b9a0b9445fbe38d2955fd3f2182f0f5f29
Jonathan Lignier <jonathan0780@hotmail.fr>
2019-11-29 14:42:29 +0100

    Q1 : class Date with getNbDays function

--- date.cpp
+++ date.cpp
@@ -1,8 +1,12 @@
 
 #include "date.hpp"
+#include <iostream>
 
+Date::Date(){
 
-Date::Date (int day, int month, int year) {
+}
+
+Date::Date(int day, int month, int year) {
 	m_month = month;
 	m_day = day;
 	m_year = year;
@@ -18,4 +22,106 @@ int Date::getDay() {
 
 int Date::getYear() {
 	return m_year;
+}
+
+int Date::getDaysInMonth(int m, int year) {
+	int nb;
+
+	switch (m) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			nb = 31;
+			break;
+		case 2:
+			if (bissextile(year)) {
+				nb = 29;
+			} else {
+				nb = 28;
+			}
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+		default:
+			nb = 30;
+			break;
+	}
+	return nb;
+}
+
+bool Date::bissextile(int year){
+	if (year%400 == 0 || (year%4 == 0 && year%100 != 0)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool Date::isEqual(Date d) {
+	bool res = true;
+	if (d.getDay() != m_day) {
+		res = false;
+	}
+	if (d.getMonth() != m_month) {
+		res = false;
+	}
+	if (d.getYear() != m_year) {
+		res = false;
+	}
+	return res;
+}
+bool Date::isAnterior(Date d) {
+	bool res = false;
+	if (m_year < d.getYear()) {
+		res = true;
+	} else {
+		if (m_year == d.getYear()){
+			if (m_month < d.getMonth()) {
+				res = true;
+			} else {
+				if (m_month == d.getMonth()) {
+					if (m_day < d.getDay()) {
+						res = true;
+					} else {
+						res = false;
+					}
+
+				}
+			}
+		}
+	}
+	return res;
+}
+
+int Date::getNbDays(Date d2) {
+	int nbDays = 0;
+    if (!d2.isAnterior(*this)) {
+
+		int currentDay = m_day ;
+		int currentMonth = m_month ;
+		int currentYear = m_year ;
+		Date date = Date(currentDay, currentMonth, currentYear);
+	    while ( !date.isEqual(d2) ) {
+			nbDays ++;
+			currentDay++;
+			if (currentDay > getDaysInMonth(currentMonth, currentYear) ) {
+				currentDay = 1;
+				currentMonth++;
+			}
+			if (currentMonth > 12) {
+				currentMonth = 1;
+				currentYear ++;
+			}
+			date = Date(currentDay,currentMonth, currentYear);
+		}
+	} else {
+		nbDays = -1;
+	}
+	return nbDays;
 }
--- date.hpp
+++ date.hpp
@@ -5,18 +5,24 @@
 
 
 class Date {
-private:
-  int m_month;
-  int m_day;
-  int m_year;
+    private:
+        int m_month;
+        int m_day;
+        int m_year;
 
+        int getDaysInMonth(int m, int year);
+        bool bissextile(int year);
+    public:
+        Date();
+        Date(int day, int month, int year);
 
-public:
-  Date (int day, int month, int year);
+        int getMonth();
+        int getDay();
+        int getYear();
+        bool isEqual(Date d);
+        bool isAnterior(Date d);
+        int getNbDays(Date d2);
 
-  int getMonth();
-  int getDay();
-  int getYear();
 };
 
 #endif
